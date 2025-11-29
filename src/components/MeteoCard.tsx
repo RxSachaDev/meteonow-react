0627ec116dec0ca016235f3f01b099b3fef80e84
RxSachaDev <sacha.work38@gmail.com>
2025-11-29 18:06:30 -0500

    commencement du prenier composant

---
+++ src/components/MeteoCard.tsx
@@ -0,0 +1,15 @@
+
+
+const MeteoCard = () => {
+    return (
+        <div className="pl-2 bg-[#F5F5F5]">
+            <h3>Montréal</h3>
+            <h2>1.38°C</h2>
+            <p>Wind : 20 km/h</p>
+            <p>Humidity : 10%</p>
+            <h4>Snowly</h4>
+        </div>
+    )
+}   
+
+export default MeteoCard;
