b13e1d19d2496f8dc7edb644c097665b8c7b0e8d
RxSachaDev <sacha.work38@gmail.com>
2025-11-30 14:46:09 -0500

    fin des différents composants

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import MeteoCard from "./components/MeteoCard"
 import MeteoNextHours from "./components/MeteoNextHours"
+import MeteoNextDays from "./components/MeteoNextDays"
+import CityInput from "./components/CityInput"
 
 function App() {
 
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-5">
         <h1>Météo</h1>
       </div>
+      <section className="flex justify-center mt-10">
+        <CityInput />
+      </section>
       <section className="flex gap-6 justify-center mt-15" >
         <MeteoCard />
-        <MeteoNextHours />
+        <MeteoNextDays />
+      </section>
+      <section className="flex justify-center">
+        <MeteoNextHours />        
       </section>
     </>
   )
--- src/components/MeteoNextDays.tsx
+++ src/components/MeteoNextDays.tsx
@@ -1,35 +1,36 @@
 import { FaSun } from "react-icons/fa6";
 
-const MeteoNextHours = () => {
+const MeteoNextDays = () => {
     return (
-        <div className="bg-color-default w-225 rounded-3xl mt-5 shadow-lg h-45 flex justify-center items-center gap-12">
+        <div className="bg-color-default w-140 rounded-3xl flex shadow-lg gap-12 justify-center items-center">
             <div className="flex flex-col items-center gap-2">
                 <h4 className="font-semibold">21°C</h4>
                 <FaSun size={40} className="text-gray-400"/>
-                <h3 className="font-semibold">Mond</h3>
+                <h3 className="font-semibold">Sun</h3>
             </div>
             <div className="flex flex-col items-center gap-2">
-                <h4 className="font-semibold">21°C</h4>
+                <h4 className="font-semibold">23°C</h4>
                 <FaSun size={40} className="text-gray-400"/>
-                <h3 className="font-semibold">Mond</h3>
+                <h3 className="font-semibold">Mon</h3>
             </div>
             <div className="flex flex-col items-center gap-2">
-                <h4 className="font-semibold">21°C</h4>
+                <h4 className="font-semibold">22°C</h4>
                 <FaSun size={40} className="text-gray-400"/>
-                <h3 className="font-semibold">Mond</h3>
+                <h3 className="font-semibold">Wed</h3>
             </div>
             <div className="flex flex-col items-center gap-2">
-                <h4 className="font-semibold">21°C</h4>
+                <h4 className="font-semibold">22°C</h4>
                 <FaSun size={40} className="text-gray-400"/>
-                <h3 className="font-semibold">Mond</h3>
+                <h3 className="font-semibold">Wed</h3>
             </div>
             <div className="flex flex-col items-center gap-2">
-                <h4 className="font-semibold">21°C</h4>
+                <h4 className="font-semibold">22°C</h4>
                 <FaSun size={40} className="text-gray-400"/>
-                <h3 className="font-semibold">Mond</h3>
+                <h3 className="font-semibold">Wed</h3>
             </div>
         </div>
+        
     )
 }
 
-export default MeteoNextHours
+export default MeteoNextDays
